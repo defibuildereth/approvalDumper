37580b03c81ff3130fd2763c590f95cb5cfa2ab1
d7o1d1s0 <d7sarmy@gmail.com>
2024-03-31 22:15:07 +0100

    console logging contract events

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 require('dotenv').config()
-const providers = require("./providers.js")
 
 let privatekey = process.env.PRIVATE_KEY
 
+
 let aeroTokenAddress = '0x940181a94a35a4569e4529a3cdfb74e38fd98631'
-let usdcTokenAddress = '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913'
 let targetWalletAddress = '0x9efc025f46f77003C71cce8E670FB9cCEd89fee0'
 
-let condition = {
-    address: usdcTokenAddress
+
+async function checkContract() {
+    let res = await fetch(`https://api.basescan.org/api?module=account&action=txlist&address=0x940181a94a35a4569e4529a3cdfb74e38fd98631&startblock=0&endblock=99999999&page=1&offset=10&sort=desc&apikey=${process.env.BASESCAN_API_KEY}`)
+    let r = await res.json()
+    console.log(r)
 }
 
 async function botGo() {
     console.log('running')
 
-    providers.BASE.on(condition, 
-        (event) => {
-            try {
-                console.log(event)
-            }
-            catch(err) {
-                console.log(error)
-            }
-        })
+
 }
 
-botGo()
+checkContract()
